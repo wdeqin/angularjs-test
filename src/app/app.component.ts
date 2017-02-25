6a432b7c5031f11542b25938c98e859a7d53dc69
smalacnt <smalacnt@gmail.com>
2017-02-25 02:49:37 -0500

    simplified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
-  selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <div class="grid grid-pad">
-    <a *ngFor="let hero of heroes"  [routerLink]="['/detail', hero.id]"  class="col-1-4">
-    <div class="module hero">
-      <h4>{{hero.name}}</h4>
-    </div>
-    </a>
-    </div>
-    <hero-search></hero-search>
-    <nav>
-      <a routerLink="/dashboard">Dashboard</a>
-      <a routerLink="/heros">Heros</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `,
-  styleUrls: ['./app.component.css',],
+    moduleId: module.id,
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css',],
 })
 
 export class AppComponent {
-  title = 'Tour of Heros';
+    inputText: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { HttpModule } from '@angular/http';
-
-// Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
-
-import { HerosComponent } from './heros.component';
-import { DashboardComponent } from './dashboard.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroSearchComponent } from './hero-search.component';
-
-import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryDataService),
-    AppRoutingModule,
-  ],
-  declarations: [
-    AppComponent,
-    HerosComponent,
-    HeroDetailComponent,
-    DashboardComponent,
-    HeroSearchComponent,
-  ],
-  bootstrap: [
-    AppComponent,
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+    ],
+    declarations: [
+        AppComponent,
+    ],
+    bootstrap: [
+        AppComponent,
+    ],
 })
 export class AppModule { }
